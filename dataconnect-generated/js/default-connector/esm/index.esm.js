93244af8baf0605603245e6c3ae0a80333002f94
samuelwclement <samuelwclement@gmail.com>
2025-03-02 18:45:19 -0800

    finished product page

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { , validateArgs } from 'firebase/data-connect';
+
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'project-achang26uw',
+  location: 'us-central1'
+};
