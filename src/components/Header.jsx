30177c76712dadc2afd31b79d329b5ed96676e25
Shuban Burla <sburla@uw.edu>
2025-03-10 01:55:20 -0700

    Updated files for USWAP Project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Header = ({ loggedInUser, handleLogout }) => {
+const Header = () => {
   return (
-    <header className="header-container">
-      <div className="logo-container">
-        <img src="img/USWAP_logo.png" alt="USWAP Logo" className="logo" />
+    <header className="header">
+      <img src="img/USWAP_logo.png" alt="USWAP Logo" className="logo" />
         <h1>USWAP</h1>
-      </div>
+        
+         
+      
       <nav>
         <ul className="nav-links">
           <li><Link to="/">Home</Link></li>
           <li><Link to="/listings">Listings</Link></li>
           <li><Link to="/add-item">Add Item</Link></li>
           <li><Link to="/wishlist" className="wishlist-btn">Wishlist</Link></li>
-
-          {loggedInUser ? (
-            <li><button onClick={handleLogout} className="logout-btn">Logout</button></li>
-          ) : (
             <li><Link to="/login" className="login-btn">Login</Link></li>
-          )}
+  
         </ul>
       </nav>
     </header>
